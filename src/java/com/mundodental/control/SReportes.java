7d56774e5c6fbf3de3f97c871f32e91238c30a91
Nelson <Nelson@Cruz>
2019-11-29 13:26:59 -0600

    Reporte preliminar de citas

--- src/java/com/mundodental/control/SReportes.java
+++ src/java/com/mundodental/control/SReportes.java
@@ -24,10 +24,10 @@ public class SReportes extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         String accion = request.getParameter("accion");
-        if (accion == null){
+        if(accion == null){
             request.getRequestDispatcher("reportes.jsp").forward(request, response);
         }
-        // 
+        // TOTAL DE LAS CONSULTAS
         else if(accion.equals("totalConsultas")) {
             Conexion cn = new ConexionPool();
             cn.conectar();
@@ -50,7 +50,7 @@ public class SReportes extends HttpServlet {
             }
             response.setContentType("application/pdf");
             //para que el pdf se pueda ver en microsoft explorer
-            //response.setHeader("Cache-Control", "cache");
+            response.setHeader("Cache-Control", "cache");
             //para que aparezca el diálogo abrir/guardar
             //response.setHeader("Content-Disposition", "attachment; filename=reporte.pdf");
             response.setHeader("Content-Disposition", "inline; filename=Reportes.pdf");
@@ -68,12 +68,12 @@ public class SReportes extends HttpServlet {
 
             ServletContext context = request.getServletContext();
             //obtenemos el reporte (archivo .jasper) en un archivo de tipo file
-            File reportFile = new File(context.getRealPath("/") + "reportes/Reportes.jasper");
+            File reportFile = new File(context.getRealPath("/") + "reportes/totalCitas.jasper");
 
             //configuramos los parametros en una varible de tipo Map
             //que como un array
             Map parameters = new HashMap();
-            parameters.put("origen", "1");
+            //parameters.put("origen", "1");
             //para agregar mas parametro solo llame el metodo put() varias veces
             byte[] bytes = null;
             try {
